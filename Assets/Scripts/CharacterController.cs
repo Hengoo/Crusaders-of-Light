a0b90e4d0d76b82a36a85c29979ef5aa2327e8a1
Hengo <leitner.andi@googlemail.com>
2017-10-19 11:56:54 +0200

    First CharacterController test

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+	PIDControllerVel velPID;
+	PIDControllerRot rotPID;
+	Rigidbody rbody;
+
+	// Use this for initialization
+	void Start()
+	{
+		rbody = this.gameObject.GetComponent<Rigidbody>();
+		velPID = new PIDControllerVel(this.gameObject, 10, 0, 0.5f, new Vector3(1, 1, 1), 50f);
+		//rotPID = new PIDControllerRot(this.gameObject, 100, 20 , 33);
+		rotPID = new PIDControllerRot(this.gameObject, 38, 1, 14);
+
+		inizializeRigidbody();
+	}
+
+	/// <summary>
+	/// sets innertia tensor to (1,1,1) and center of mass to (0,0,0)
+	/// </summary>
+	private void inizializeRigidbody()
+	{
+		rbody.centerOfMass = Vector3.zero;
+		rbody.inertiaTensor = Vector3.one * rbody.mass;
+		rbody.maxAngularVelocity = 25;
+
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		float speedfaktor = 10;
+		Vector3 targetVel = new Vector3(Input.GetAxis("Horizontal") * speedfaktor, 0, Input.GetAxis("Vertical") * speedfaktor);
+		velPID.UpdateTarget(targetVel, 1);
+
+		Quaternion targetRot;
+		Vector3 rightStick = new Vector3(Input.GetAxis("Horizontal2"), 0, -Input.GetAxis("Vertical2"));
+		targetRot = Quaternion.LookRotation(rightStick, Vector3.up);
+		rotPID.UpdateTarget(targetRot);
+	}
+}
---
+++ Assets/Scripts/PID/VectorPid.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// PID controller with 3 values at once
+/// works like 3 pid controllers with 1 value each
+/// </summary>
+public class VectorPid : MonoBehaviour
+{
+	public float pFactor, iFactor, dFactor;
+
+	protected Vector3 integral;
+	protected Vector3 lastError;
+
+	public VectorPid(float pFactor, float iFactor, float dFactor)
+	{
+		this.pFactor = pFactor;
+		this.iFactor = iFactor;
+		this.dFactor = dFactor;
+	}
+
+	public virtual Vector3 UpdateNormalPid(Vector3 currentError, float timeFrame, Vector3 target, Vector3 current)
+	{
+		integral += currentError * timeFrame;
+		var deriv = (currentError - lastError) / timeFrame;
+		lastError = currentError;
+		return currentError * pFactor
+			+ integral * iFactor
+			+ deriv * dFactor;
+	}
+
+	public virtual Vector3 UpdateModifiedPid(Vector3 currentError, float timeFrame)
+	{
+		//reduce integrall with time to eliminate huge error when walking moving against walls, ez;
+		//only neccesarry for rotations since the axis can change
+		integral *= 0.98f;
+
+		integral += currentError * timeFrame;
+		var deriv = (currentError - lastError) / timeFrame;
+		lastError = currentError;
+		return currentError * pFactor
+			+ integral * iFactor
+			+ deriv * dFactor;
+	}
+}
