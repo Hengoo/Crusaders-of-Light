103b8c5d8970db403959e3ab96f518879f1088cd
Jean <jeanfilho@gmail.com>
2017-10-25 22:24:43 +0200

    finished implementation of delaunay triangulations using Bowyer-Watson

--- Assets/Scripts/TerrainStructure.cs
+++ Assets/Scripts/TerrainStructure.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class TerrainStructure : MonoBehaviour
+public class TerrainStructure
 {
     private readonly Graph<BiomeData> _biomes = new Graph<BiomeData>();
     private readonly List<int> _biomeIDs = new List<int>();
 
-    public TerrainStructure(List<BiomeData> biomes)
+    public TerrainStructure(List<BiomeData> biomes, int height, int width)
     {
         //Add all points to the graph
         foreach (var biome in biomes)
@@ -14,8 +15,10 @@ public class TerrainStructure : MonoBehaviour
             _biomeIDs.Add(_biomes.AddNode(biome));
         }
 
-        List<Triangle> triangles = DelaunayTriangulation();
+        // Calculate connectivity between biome
+        List<Triangle> triangles = DelaunayTriangulation(height, width);
 
+        // Add biome connectivity to graph
         foreach (var triangle in triangles)
         {
             _biomes.AddEdge(triangle.P0, triangle.P1, 1);
@@ -24,11 +27,79 @@ public class TerrainStructure : MonoBehaviour
         }
     }
 
+    private List<Triangle> DelaunayTriangulation(int height, int width)
+    {
+        // Encompassing biomes for Bowyer-Watson
+        var left = _biomes.AddNode(new BiomeData(new Vector2(-width, -height / 2f), 0, 0, 0));
+        var right = _biomes.AddNode(new BiomeData(new Vector2(width, -height / 2f), 0, 0, 0));
+        var top = _biomes.AddNode(new BiomeData(new Vector2(0, height * 1.5f), 0, 0, 0));
+
+        // Add super triangle
+        List<Triangle> result = new List<Triangle> {new Triangle(left, top, right)};
+        HashSet<int> toRetriangulate = new HashSet<int>();
+
+        // Bowyer-Watson - iterate through all points
+        foreach (var biomeID in _biomeIDs)
+        {
+            // Skip super triangle vertices
+            if (biomeID == left || biomeID == right || biomeID == top)
+                continue;
+
+            Vector2 point = _biomes.GetNodeData(biomeID).Center;
+            toRetriangulate.Clear();
+            foreach (var triangle in result)
+            {
+                // Check which triangles need to be removed
+                if (IsInCircumcircle(point, triangle))
+                {
+                    result.Remove(triangle);
+                    toRetriangulate.Add(triangle.P0);
+                    toRetriangulate.Add(triangle.P1);
+                    toRetriangulate.Add(triangle.P2);
+                }
+            }
+            // Add new triangles connecting to the new point
+            var points = toRetriangulate.ToArray();
+            for (int i = 0; i < points.Length; i++)
+            {
+                result.Add(new Triangle(biomeID, points[i], points[(i + 1) % points.Length]));
+            }
+        }
+
+        // Remove super triangle
+        _biomes.RemoveNode(left);
+        _biomes.RemoveNode(right);
+        _biomes.RemoveNode(top);
+        foreach (var triangle in result)
+            if (triangle.Contains(left) || triangle.Contains(right) || triangle.Contains(top))
+                result.Remove(triangle);
+
+        return result;
+    }
+
+    // See http://mathworld.wolfram.com/TriangleInterior.html
+    private bool IsInCircumcircle(Vector2 v, Triangle triangle)
+    {
+        var v0 = _biomes.GetNodeData(triangle.P0).Center;
+        var v1 = _biomes.GetNodeData(triangle.P1).Center - v0;
+        var v2 = _biomes.GetNodeData(triangle.P2).Center - v0;
+
+        var a = (Det(v, v2) - Det(v0, v2)) / Det(v1, v2);
+        var b = - (Det(v, v1) - Det(v0, v1)) / Det(v1, v2);
+
+        return a > 0 && b > 0 && a + b >0;
+    }
+
+    private float Det(Vector2 left, Vector2 right)
+    {
+        return left.x * right.y - left.y * right.x;
+    }
+
     public BiomeSample SampleBiomeData(Vector2 position)
     {
         //TODO implement sampling
         return new BiomeSample(0, 0);
-    }    
+    }
 
     private struct Triangle
     {
@@ -41,19 +112,11 @@ public class TerrainStructure : MonoBehaviour
             P1 = p1;
             P2 = p2;
         }
-    }
 
-    private List<Triangle> DelaunayTriangulation()
-    {
-        List<Triangle> result = new List<Triangle>();
-
-        //TODO calculate delaunay triangulation biome centers
-        foreach (var biomeID in _biomeIDs)
+        public bool Contains(int id)
         {
-            Vector2 center = _biomes.GetNodeData(biomeID).Center;
+            return id == P0 || id == P1 || id == P2;
         }
-
-        return result;
     }
 }
 
