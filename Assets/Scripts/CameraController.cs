c3c52369da8cd4f67282268b73ec99b702f9b7f0
Hengo <leitner.andi@googlemail.com>
2017-12-01 18:46:23 +0100

    Small Camera Script (future todo: probably want to angle the camera a bit, but lets see)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject[] cameraTargets;
+	public float distanceMin;
+	public float distanceMultiplier;
+	public float distanceMax;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float distance = 0;
+		Vector3 averagePos = Vector3.zero;
+		foreach (GameObject target in cameraTargets)
+		{
+			averagePos += target.transform.position;
+		}
+		averagePos /= cameraTargets.Length;
+		foreach (GameObject target in cameraTargets)
+		{
+			distance += (averagePos - target.transform.position).magnitude;
+		}
+		distance = distance * distanceMultiplier;
+		distance = Mathf.Min(distanceMax, distance);
+		distance = Mathf.Max(distanceMin, distance);
+		this.transform.position = averagePos + Vector3.up * distance;
+	}
+}
