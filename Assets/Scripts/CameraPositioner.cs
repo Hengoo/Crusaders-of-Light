bd01ef4ef1e5d39d439ababe2facebd01b52867a
Hengo <leitner.andi@googlemail.com>
2017-12-01 18:52:30 +0100

    Renamed script to avoid name collision

--- Assets/Scripts/CameraPositioner.cs
+++ Assets/Scripts/CameraPositioner.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraPositioner: MonoBehaviour
 {
 	public GameObject[] cameraTargets;
 	public float distanceMin;
