2d9b4ff9ccd02ee7e1ed56749c36ed6b59679bca
Jean <jeanfilho@gmail.com>
2018-01-07 22:26:38 +0100

    Bugfix: Fixed double invoke on quest reach place

--- Assets/Scripts/QuestSystem/QuestTypes/QuestReachPlaceTrigger.cs
+++ Assets/Scripts/QuestSystem/QuestTypes/QuestReachPlaceTrigger.cs
@@ -15,7 +15,10 @@ public class QuestReachPlaceTrigger : MonoBehaviour
         if (!other.gameObject.name.ToLower().Contains("player")) return;
 
         if (TriggerEnterAction != null)
+        {
             TriggerEnterAction.Invoke();
+            TriggerEnterAction = null;
+        }
         else
             Debug.LogWarning("No action added to trigger at GameObject: " + gameObject.name);
     }
