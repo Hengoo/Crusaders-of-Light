ed713a4e38d9fb12845a9bca085ca8da40f36b41
Hengo <leitner.andi@googlemail.com>
2018-01-09 01:04:05 +0100

    First brightness script tests

---
+++ Assets/Scripts/BrightnessChange.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.PostProcessing;
+
+//copied from here https://github.com/Unity-Technologies/PostProcessing/wiki/(v1)-Runtime-post-processing-modification
+
+[RequireComponent(typeof(PostProcessingBehaviour))]
+public class BrightnessChange : MonoBehaviour
+{
+	PostProcessingProfile m_Profile;
+
+	void OnEnable()
+	{
+		var behaviour = GetComponent<PostProcessingBehaviour>();
+
+		if (behaviour.profile == null)
+		{
+			enabled = false;
+			return;
+		}
+
+		m_Profile = Instantiate(behaviour.profile);
+		behaviour.profile = m_Profile;
+	}
+
+	void Update()
+	{
+		//var vignette = m_Profile.vignette.settings;
+		//vignette.smoothness = Mathf.Abs(Mathf.Sin(Time.realtimeSinceStartup) * 0.99f) + 0.01f;
+		//m_Profile.vignette.settings = vignette;
+
+		var colorGrading = m_Profile.colorGrading.settings;
+		colorGrading.basic.postExposure = 0.4f;
+		m_Profile.colorGrading.settings = colorGrading;
+	}
+}
