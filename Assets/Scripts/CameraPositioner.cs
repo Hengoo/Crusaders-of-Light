a2ec77cf35f02ec4d711ec679093021557f2ebd3
Florian <f.buschek@gmail.com>
2018-01-09 17:16:34 +0100

    Changed Camera to FixedUpdate. Added Player Prefabs to Scene and Camera.

--- Assets/Scripts/CameraPositioner.cs
+++ Assets/Scripts/CameraPositioner.cs
@@ -16,7 +16,7 @@ public class CameraPositioner : MonoBehaviour
 	}
 
 	// Update is called once per frame
-	void Update()
+	void FixedUpdate()
 	{
         if (cameraTargets.Length <= 0)
         {
--- Assets/Scripts/QuestSystem/Puzzles/AreaIceWall.cs
+++ Assets/Scripts/QuestSystem/Puzzles/AreaIceWall.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor;
 using UnityEngine;
 /*
  * This class creates the Ice Wall area
