fa5688c219e41de0f2cf2a331b07267b20493f59
Hengo <leitner.andi@googlemail.com>
2018-01-12 15:26:56 +0100

    Added live menu

---
+++ Assets/Scripts/MenuLivePreview.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class MenuLivePreview : MonoBehaviour
+{
+	public GameObject mainCamera;
+	public GameObject[] characters;
+
+	public PostProcessingProfile m_Profile;
+
+	void Start()
+	{
+		var behaviour = mainCamera.GetComponent<PostProcessingBehaviour>();
+		//var behaviour = GetComponent<PostProcessingBehaviour>();
+
+		if (behaviour.profile == null)
+		{
+			return;
+		}
+
+		m_Profile = Instantiate(behaviour.profile);
+		behaviour.profile = m_Profile;
+	}
+
+
+	public void UpdatePlayerCount(string count)
+	{
+		int intCount = int.Parse(count);
+		for (int i = 0; i < 4; i++)
+		{
+			characters[i].SetActive(i < intCount);
+		}
+	}
+
+	public void UpdateBrightness(float brightness)
+	{
+		if(m_Profile ==null)
+		{
+			//i believe this should never get here
+			var behaviour = mainCamera.GetComponent<PostProcessingBehaviour>();
+			//var behaviour = GetComponent<PostProcessingBehaviour>();
+			m_Profile = Instantiate(behaviour.profile);
+			behaviour.profile = m_Profile;
+		}
+
+		var colorGrading = m_Profile.colorGrading.settings;
+		colorGrading.basic.postExposure = brightness * 3;
+		m_Profile.colorGrading.settings = colorGrading;
+	}
+
+}
