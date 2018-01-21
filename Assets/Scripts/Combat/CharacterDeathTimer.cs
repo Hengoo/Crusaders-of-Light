b407257da406beca4cd5066ca63b60318b5b8f5d
Florian <f.buschek@gmail.com>
2018-01-21 12:53:00 +0100

    Animations now properly reset when dying, along with a bunch of other values. Added a DeathTimer Script that is used as a timer for how long a dead player falls around after dying until being frozen in place.

---
+++ Assets/Scripts/Combat/CharacterDeathTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterDeathTimer : MonoBehaviour {
+
+    [Header("Character Death Timer:")]
+
+    public float DeathDuration = 1f;
+    private float DeathCounter = 0f;
+
+    private void Update()
+    {
+        DeathCounter += Time.deltaTime;
+
+        if (DeathCounter >= DeathDuration)
+        {
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            this.enabled = false;
+        }
+    }
+
+    public void StartDeathTimer()
+    {
+        DeathCounter = 0f;
+        this.enabled = true;
+    }
+}
