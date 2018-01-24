edda273d4a0d7fd9c169d03ac05f4331dae6b42f
Jean <jeanfilho@gmail.com>
2018-01-24 23:00:35 +0100

    removed unused variables

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -7,9 +7,6 @@ using UnityEngine.SceneManagement;
 
 public class LevelController : Singleton<LevelController>
 {
-    public List<AreaBase> AreaPuzzlePool; //Pool of quests for an entire area available from start
-    public List<AreaBase> QuestBossPool; //Pool of quests for the last area, aka Boss Area
-
     public QuestController QuestController;
 
     public CharacterPlayer[] PlayerCharacters;
