6d3e919765d45439159ae39979d0aa03419a4f14
Hengo <leitner.andi@googlemail.com>
2018-05-21 15:59:03 +0200

    Added new post processing and adjusted brightnes change

--- Assets/Scripts/Menu/MenuLivePreview.cs
+++ Assets/Scripts/Menu/MenuLivePreview.cs
@@ -8,20 +8,9 @@ public class MenuLivePreview : MonoBehaviour
 	public GameObject mainCamera;
 	public GameObject[] characters;
 
-	public PostProcessingProfile m_Profile;
-
 	void Start()
 	{
-		var behaviour = mainCamera.GetComponent<PostProcessingBehaviour>();
-		//var behaviour = GetComponent<PostProcessingBehaviour>();
 
-		if (behaviour.profile == null)
-		{
-			return;
-		}
-
-		m_Profile = Instantiate(behaviour.profile);
-		behaviour.profile = m_Profile;
 	}
 
 
@@ -36,18 +25,8 @@ public class MenuLivePreview : MonoBehaviour
 
 	public void UpdateBrightness(float brightness)
 	{
-		if(m_Profile ==null)
-		{
-			//i believe this should never get here
-			var behaviour = mainCamera.GetComponent<PostProcessingBehaviour>();
-			//var behaviour = GetComponent<PostProcessingBehaviour>();
-			m_Profile = Instantiate(behaviour.profile);
-			behaviour.profile = m_Profile;
-		}
-
-		var colorGrading = m_Profile.colorGrading.settings;
-		colorGrading.basic.postExposure = brightness * 3;
-		m_Profile.colorGrading.settings = colorGrading;
+		RenderSettings.ambientSkyColor = Color.Lerp(Color.black, Color.white, brightness);
+		print("test");
 	}
 
 }
