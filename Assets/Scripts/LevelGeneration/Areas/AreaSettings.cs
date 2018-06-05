273cb83c2d7bd7dab07045e42996c50dc6404db5
Jean <jeanfilho@gmail.com>
2018-06-05 15:01:14 +0200

    restructured folders

---
+++ Assets/Scripts/LevelGeneration/Areas/AreaSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AreaSettings
+{
+    public string Name = "Area";
+    public List<PoissonDiskFillData> PoissonDataList = new List<PoissonDiskFillData>();
+
+    public Graph<AreaData> AreaDataGraph { get; protected set; }
+    public Vector2[][] ClearPolygons { get; protected set; }
+    public Vector2[] BorderPolygon { get; protected set; }
+
+    public abstract GameObject GenerateAreaScenery(Terrain terrain);
+}
+
+public class AreaData
+{
+    public Vector2 Center;
+    public AreaSegment Segment;
+    public Vector2[] Polygon;
+}
+
+//-------------------------------------------------------------------------------------
+//
+//                                AREA SETTINGS
+//
+//-------------------------------------------------------------------------------------
---
+++ Assets/Scripts/LevelGeneration/Areas/AreaSettingsFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Xml;
+using UnityEngine;
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable FieldCanBeMadeReadOnly.Global
+
+public abstract class AreaSettingsFactory : ScriptableObject
+{
+    public abstract Graph<AreaSegment> GetPatternGraph();
+    public abstract AreaSettings ProduceAreaSettings(Graph<AreaData> areaDataGraph, IEnumerable<Vector2[]> clearPolygons, Vector2[] borderPolygon);
+}
