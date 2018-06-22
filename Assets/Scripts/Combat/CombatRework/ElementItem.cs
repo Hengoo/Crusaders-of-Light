881ca56479bffa39b103205a74521e5435a8ceef
Florian <f.buschek@gmail.com>
2018-06-22 22:30:44 +0200

    All Elements and Weapons finished for now, except that Frost cant slow yet.

--- Assets/Scripts/Combat/CombatRework/ElementItem.cs
+++ Assets/Scripts/Combat/CombatRework/ElementItem.cs
@@ -10,11 +10,12 @@ public class ElementItem : MonoBehaviour {
         BASIC_2 = 1,
         SPECIAL_1 = 2,
         SPECIAL_2 = 3,
-        SPECIAL_3 = 4
+        SPECIAL_3 = 4,
+        SPELL_1 = 5
     }
 
     [Header("Bonus Effects:")]
-    public ElementBonusEffect[] BonusEffects = new ElementBonusEffect[5];   // Needs to be exactly as big as there are effectTypes!
+    public ElementBonusEffect[] BonusEffects = new ElementBonusEffect[6];   // Needs to be exactly as big as there are effectTypes!
 
     public ElementBonusEffect GetBonusEffectOfType(EffectType EType)
     {
