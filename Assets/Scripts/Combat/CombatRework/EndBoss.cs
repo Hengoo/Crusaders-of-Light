2551936fef03c0cd68b4928ccbb25d290701de30
Florian <f.buschek@gmail.com>
2018-06-29 15:28:36 +0200

    Flame Wizard now drops Fire Element. Golem now drops Frost Element. (Not visually...)

---
+++ Assets/Scripts/Combat/CombatRework/EndBoss.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndBoss : MonoBehaviour {
+
+    public ElementItem element;
+
+    private void OnDestroy()
+    {
+        if (GameController.Instance)
+        {
+            GameController.Instance.UnlockElement(element);
+        }
+    }
+}
